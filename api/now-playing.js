9c74ea5d921de42a30aca4578557ff26c2113098
hquirogab <42346378+hquirogab@users.noreply.github.com>
2026-04-29 01:55:51 -0500

    Update now-playing.js

--- api/now-playing.js
+++ api/now-playing.js
@@ -1,26 +1,11 @@
 export default async function handler(req, res) {
-  // 1. Set CORS headers so your GitHub blog can talk to Vercel
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  const { LASTFM_USER, LASTFM_KEY } = process.env;
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${LASTFM_USER}&api_key=${LASTFM_KEY}&format=json&limit=1`;
 
-  const username = "hquiroga";
-  const apiKey = process.env.LASTFM_API_KEY; // We will set this in Vercel
-  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json&limit=1`;
+  const response = await fetch(url);
+  const data = await response.json();
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    
-    // Send the data back to your blog
-    return res.status(200).json(data);
-  } catch (error) {
-    return res.status(500).json({ error: "Failed to fetch from Last.fm" });
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
 }
